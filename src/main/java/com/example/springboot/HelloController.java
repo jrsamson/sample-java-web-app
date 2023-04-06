c9b50e302a4c3601775f1d5377abdf3351977757
Floro Samson <bsamson@bsamsonWNPHQ.vmware.com>
2023-04-05 20:50:19 -0600

    chg

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,16 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+	//public String hello(@RequestParam(value = "name", defaultValue = "") String name) {
+	public String index(@RequestParam(value="name", defaultValue="") String name)  {
+		return String.format("Greetings %s from Spring Boot + Tanzu!",name);
+		//return String.format("Hello %s!", name);
 	}
 
 }
